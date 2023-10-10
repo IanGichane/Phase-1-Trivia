05a1c917c3deb9b1b401632209f9ce7bb645723e
IanGichane <gichaneian@gmail.com>
2023-10-10 16:32:26 +0300

    Retrieve data from the Api using fetch

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 //console.log('hello')
+const trivia =
+  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean";
+
+console.log("hello");
+function getTrivia(result) {
+  console.log(result);
+  console.log("T");
+}
+console.log("hello");
+
+function getData() {
+  fetch(trivia)
+    .then((response) => response.json())
+    .then((dt) => {
+      let result = dt.results;
+      // let results = dt.results;
+      console.log(result);
+      result.forEach((trivaQues) => {
+        getTrivia(trivaQues);
+      });
+    })
+    // .then((data) =>
+    //   data.forEach(
+    //     // console.log('hello')
+    //     (trivQues) => {
+    //       getTrivia(trivQues);
+    //     }
+    //   )
+    // )
+    .catch((err) => console.log("error", err));
+}
+getData();
