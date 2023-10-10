f95903781ddab60f2762f30fcb3dc7cf8cc1ee2d
IanGichane <gichaneian@gmail.com>
2023-10-10 23:59:02 +0300

    Efficient and reliable way to pick one question at a time

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 //console.log('hello')
 const trivia =
   "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean";
+//Display question to the Dom
 
-console.log("hello");
-function getTrivia(trivaQues) {
-  let list = document.createElement("li");
-  list.textContent = `${trivaQues.question}`;
-  document.querySelector(".container").appendChild(list);
-
-  console.log(list);
-  console.log(result);
-  console.log("T");
+function question(result) {
+  let i = Math.floor(Math.random() * 10);
+  console.log(i);
+  console.log(result[i].question);
+  let triviaQuestion = document.querySelector("#question");
+  triviaQuestion.innerText = `${result[i].question} ${result[i].correct_answer}`;
+  if (result[i].correct_answer === "True") {
+    console.log("You are right");
+  } else {
+    console.log("Oh No!");
+  }
 }
-console.log("hello");
 
 function getData() {
   fetch(trivia)
@@ -20,19 +22,10 @@ function getData() {
     .then((dt) => {
       let result = dt.results;
       // let results = dt.results;
-      console.log(result);
-      result.forEach((trivaQues) => {
-        getTrivia(trivaQues);
-      });
+      console.log(result[1]);
+      question(result);
     })
-    // .then((data) => data is not an array but data,results is
-    //   data.forEach(
-    //     // console.log('hello')
-    //     (trivQues) => {
-    //       getTrivia(trivQues);
-    //     }
-    //   )
-    // )
+
     .catch((err) => console.log("error", err));
 }
 getData();
