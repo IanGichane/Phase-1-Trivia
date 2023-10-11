a8210829f279b756d341aac7b7e01232ad611b76
IanGichane <gichaneian@gmail.com>
2023-10-11 14:38:34 +0300

    Added true or false buttons to select option

--- index.js
+++ index.js
@@ -9,20 +9,35 @@ function question(result) {
   console.log(result[i].question);
   let triviaQuestion = document.querySelector("#question");
   triviaQuestion.innerText = `${result[i].question} ${result[i].correct_answer}`;
-  if (result[i].correct_answer === "True") {
-    console.log("You are right");
-  } else {
-    console.log("Oh No!");
-  }
+
+  // if (result[i].correct_answer === "True") {
+  //   console.log("You are right");
+  // } else {
+  //   console.log("Oh No!");
+  // }
+  document.querySelector("#True").addEventListener("click", () => {
+    if (result[i].correct_answer === "True") {
+      console.log("You are right");
+    } else {
+      console.log("Oh No!");
+    }
+  });
+  document.querySelector("#False").addEventListener("click", () => {
+    if (result[i].correct_answer === "False") {
+      console.log("Oh No!");
+    } else {
+      console.log("but its true right");
+    }
+  });
 }
 
-function getData() {
+function getData(limit = 2) {
   fetch(trivia)
     .then((response) => response.json())
     .then((dt) => {
       let result = dt.results;
       // let results = dt.results;
-      console.log(result[1]);
+      //console.log(result[1]);
       question(result);
     })
 
