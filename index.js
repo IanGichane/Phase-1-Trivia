5013b948c208ffb158bc6e605e81af8e09028507
IanGichane <gichaneian@gmail.com>
2023-10-13 11:22:09 +0300

    Working with css

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let response = document.querySelector("#response");
 
 //counter for score
 let score = 0;
+console.log(`1 ${score}` )
+
+//buttong choice select
 let trueChoice = "True";
 let falseChoice ="False"
 
@@ -12,13 +15,45 @@ let falseChoice ="False"
 setInterval(counter, 1000);
 
 //update the score counter
-function updateScore() {
-  score++;
-  document.getElementById("scoreCounter").textContent = score;
+
+console.log(`2 ${score}`)
+document.getElementById("scoreCounter").textContent = score;
   // scoreBoard.textContent = score;
-}
+  console.log(`3 ${score}`)
 
 //Display question to the Dom
+
+function counter() {
+  let count = document.querySelector("#counter");
+  count.innerHTML--;
+  if (count.textContent < 0) {
+    count.innerHTML = 3;
+    getData();
+    console.log(`1 ${score}` )
+  }
+  response.innerHTML = "";
+}
+
+//handle true button
+function handleTrue(result, i) {
+  if (result[i].correct_answer === trueChoice) {
+    response.innerHTML = `<h2>That is Correct</h2>`;
+    score+=1;
+  } else {
+    // console.log("Oh No!");
+    response.innerHTML = `<h2>Oh No!</h2>`;
+  }
+}
+
+//handle false button
+function handleFalse(result, i) {
+  if (result[i].correct_answer === falseChoice) {
+    response.innerHTML = `<h2>That is Correct</h2>`;
+    score+=1
+  } else {
+    response.innerHTML = `<h2>Oh No!</h2>`;
+  }
+}
 function question(result) {
   let i = Math.floor(Math.random() * 10);
   let triviaQuestion = document.querySelector("#question");
@@ -32,36 +67,7 @@ function question(result) {
     .addEventListener("click", () => handleFalse(result, i));
   // handleTrue(result, i);
   // handleFalse(result, i);
-}
-function counter() {
-  let count = document.querySelector("#counter");
-  count.innerHTML--;
-  if (count.textContent < 0) {
-    count.innerHTML = 3;
-    getData();
-  }
-  response.innerHTML = "";
-}
-
-//handle true button
-function handleTrue(result, i) {
-  if (result[i].correct_answer === trueChoice) {
-    response.innerHTML = `<h2>That is Correct</h2>`;
-    updateScore();
-  } else {
-    // console.log("Oh No!");
-    response.innerHTML = `<h2>Oh No!</h2>`;
-  }
-}
-
-//handle false button
-function handleFalse(result, i) {
-  if (result[i].correct_answer === falseChoice) {
-    updateScore();
-    response.innerHTML = `<h2>That is Correct</h2>`;
-  } else {
-    response.innerHTML = `<h2>Oh No!</h2>`;
-  }
+  console.log(`1 ${score}` )
 }
 
 //A counter to relod and bring up the next question when the counter reaches xero
